a4833f25b8cb7f5f55b93e32358000a6fd4bd39a
Team2Guys <faad2guys@gmail.com>
2024-05-21 07:03:29 -0700

    footer updated for mobile view

---
+++ src/data/FooterData.ts
@@ -0,0 +1,35 @@
+export const socialLinks = [
+    { href: "https://facebook.com", icon: "FaFacebookF" },
+    { href: "https://twitter.com", icon: "FaTwitter" },
+    { href: "https://instagram.com", icon: "FaInstagram" },
+    { href: "https://pinterest.com", icon: "FaPinterest" },
+  ];
+  
+  export const categories = [
+    { name: "Laptops & Computers", href: "/" },
+    { name: "Cameras & Photography", href: "#" },
+    { name: "Smart Phones & Tablets", href: "#" },
+    { name: "Video Games & Consoles", href: "#" },
+    { name: "TV & Audio", href: "#" },
+    { name: "Gadgets", href: "#" },
+    { name: "Waterproof Headphones", href: "#" },
+  ];
+  
+  export const customerCare = [
+    { name: "My Account", href: "#" },
+    { name: "Discount", href: "#" },
+    { name: "Returns", href: "#" },
+    { name: "Orders History", href: "#" },
+    { name: "Order Tracking", href: "#" },
+  ];
+  
+  export const pages = [
+    { name: "Blog", href: "#" },
+    { name: "Browse the Shop", href: "#" },
+    { name: "Category", href: "#" },
+    { name: "Pre-Built Pages", href: "#" },
+    { name: "Visual Composer Elements", href: "#" },
+    { name: "WooCommerce Pages", href: "#" },
+    { name: "WooCommerce Shortcodes", href: "#" },
+  ];
+  
