51f610bf0fde3d4d3868253b09941d8a6d71971e
AhmedSaddique <ahmed.saddique12@gmail.com>
2024-05-28 19:13:44 +0500

    productlist

--- src/components/widgets/ProductData/ProductData.tsx
+++ src/components/widgets/ProductData/ProductData.tsx
@@ -15,15 +15,15 @@ interface cardprops {
 
 const ProductData: React.FC<cardprops> = ({ productcard }) => {
   return (
-    <Container className='mt-20'>
+    <Container className='mt-20 mb-10'>
       {
         productcard.map((array, index) => (
-          <div className='border rounded-md p-4 bg-white mt-5' key={index}>
+          <div className=' shadow hover:shadow-xl rounded-md p-4 bg-white mt-5' key={index}>
             <div className={`flex flex-wrap md:flex-nowrap md:gap-5 ${index % 2 === 0 ? '' : 'md:flex-row-reverse'}`}>
               <div className='w-full md:w-3/6'>
                 <Image width={600} height={600} className='bg-contain w-full h-full' src={array.image} alt='product list' />
               </div>
-              <div className='w-full md:w-3/6 pt-4 md:pt-20 space-y-1 md:space-y-5 md:mx-10'>
+              <div className='w-full md:w-3/6 pt-4 md:pt-20 pb-3 space-y-1 md:space-y-5 md:mx-10'>
                 <h1 className='text-xl md:text-4xl font-semibold'>{array.title}</h1>
                 <p className='text-lg md:text-2xl'>{array.description}</p>
                 <div className='pt-2 md:pt-0'>
