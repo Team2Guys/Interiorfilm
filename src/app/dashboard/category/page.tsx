db970515f069fcb2341e9c33343012c83725aa32
Team2Guys <faad2guys@gmail.com>
2024-06-11 03:31:18 -0700

    add category page merge with view category

--- src/app/dashboard/category/page.tsx
+++ src/app/dashboard/category/page.tsx
@@ -4,18 +4,27 @@ import Breadcrumb from "components/Dashboard/Breadcrumbs/Breadcrumb";
 import TableTwo from "components/Dashboard/Tables/TableTwo";
 import DefaultLayout from "components/Dashboard/Layouts/DefaultLayout";
 import ProtectedRoute from "hooks/AuthHookAdmin";
-
+import { useState } from "react";
+import Addcategory from "components/AddCategory/Addcategory";
 
 const category = () => {
+
+  const [menuType, setMenuType] = useState<string>("Categories")
+
   return (
     <DefaultLayout>
-      <Breadcrumb pageName="Category" />
-
+      <Breadcrumb pageName={menuType} />
+      {
+        menuType === "Categories" ?
       <div className="flex flex-col gap-10">
-        {/* <TableOne /> */}
-        <TableTwo />
-        {/* <TableThree /> */}
+
+        <TableTwo  setMenuType={setMenuType}/>
+
       </div>
+      
+      : <Addcategory setMenuType={setMenuType}/>
+      }
+
     </DefaultLayout>
   );
 };
