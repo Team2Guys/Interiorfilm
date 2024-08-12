ad20ff865d80b947769151a4282c22b85323083f
MFaad1 <faadsardar123@gmail.com>
2024-08-12 14:14:00 +0500

    fixes

--- src/hooks/AuthHook.tsx
+++ src/hooks/AuthHook.tsx
@@ -1,22 +1,22 @@
 'use client'
 import React, { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import Loader from "components/Loader/Loader";
 import Cookies from 'js-cookie';
 import axios from "axios";
-
-import { useAppSelector } from "components/Others/HelperRedux";
 import { loggedInUserAction } from '../redux/slices/userSlice';
 import { useAppDispatch } from "components/Others/HelperRedux";
 
 function UserprotectedRoute(WrappedComponent: any) {
   const Wrapper = (props: any) => {
     const router = useRouter();
-    const [loading, setLoading] = useState<boolean>(true);
+    const [loading, setLoading] = useState<boolean>(false);
     const dispatch = useAppDispatch();
 
     const AddminProfileTriggerHandler = async (token: string | undefined | null) => {
       try {
+        setLoading(true);
+
         if(!token) return
         let user: any = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/users/getuserHandler`, {
           headers: {
@@ -30,21 +30,14 @@ function UserprotectedRoute(WrappedComponent: any) {
         setLoading(false);
       }
     }
-
+    
     useEffect(() => {
       const token = Cookies.get("user_token");
-
-      if (!token) {
-        router.push("/login");
-        setTimeout(() => {
-          setLoading(false);
-          
-        }, 1000);
-      } else {
+      if (token) {
         AddminProfileTriggerHandler(token);
-      }
+      } 
     }, [router]);
-console.log(loading, "loading")
+
     if (loading) {
       return (
         <div
