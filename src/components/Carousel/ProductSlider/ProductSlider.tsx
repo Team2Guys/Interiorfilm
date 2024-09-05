4de75cb0cddb75ab1df1039e621d9d15c9db4f09
Mujtaba Shafique <mujtaba.shafique@outlook.com>
2024-09-05 15:01:32 +0500

    Update(UI): Feature product section

--- src/components/Carousel/ProductSlider/ProductSlider.tsx
+++ src/components/Carousel/ProductSlider/ProductSlider.tsx
@@ -9,6 +9,8 @@ import Card from 'components/ui/Card/Card';
 import SkeletonLoading from 'components/Skeleton-loading/SkeletonLoading';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import PRODUCTS_TYPES from 'types/interfaces';
+import { RiArrowLeftSFill, RiArrowRightSFill } from 'react-icons/ri';
+import { BiSolidLeftArrow, BiSolidRightArrow } from 'react-icons/bi';
 
 interface ProductSliderProps {
   products: PRODUCTS_TYPES[];
@@ -37,110 +39,110 @@ const ProductSlider: React.FC<ProductSliderProps> = ({ products }) => {
     updateSwiperNavigation();
   }, [updateSwiperNavigation]);
 
- 
+
 
   return (
-<>
-{loading ? (
-      <div className="flex flex-wrap items-center justify-center gap-5 md:flex-wrap mt-5 mb-5">
-       {Array.from({ length: 3 }).map((_, index) => (
-          <div key={index} className="w-full sm:w-[48%] md:w-[20%] min-w-[250px]">
-            <SkeletonLoading
-              avatar={{
-                shape: 'square',
-                size: 300, // Will apply via classes below
-              }}
-              title={false}
-              paragraph={{ rows: 2 }}
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                gap: '10px',
-              }}
-              className="w-full"
-              active={true}
-            />
-            <div
-              className="avatar mx-auto bg-gray-300"
-              style={{
-                width: '250px',
-                height: '250px',
-              }}
-            />
+    <>
+      {loading ? (
+        <div className="flex flex-wrap items-center justify-center gap-5 md:flex-wrap mt-5 mb-5">
+          {Array.from({ length: 3 }).map((_, index) => (
+            <div key={index} className="w-full sm:w-[48%] md:w-[20%] min-w-[250px]">
+              <SkeletonLoading
+                avatar={{
+                  shape: 'square',
+                  size: 300,
+                }}
+                title={false}
+                paragraph={{ rows: 2 }}
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  gap: '10px',
+                }}
+                className="w-full"
+                active={true}
+              />
+              <div
+                className="avatar mx-auto bg-gray-300"
+                style={{
+                  width: '250px',
+                  height: '250px',
+                }}
+              />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="relative mt-2">
+          <div className='float-end flex gap-2 mb-5'>
+            <button
+              ref={prevRef}
+              className="bg-white text-black h-8 w-8 shadow-lg  flex justify-center items-center z-10 hover:scale-110 transition duration-300 ease-in-out"
+            >
+              <RiArrowLeftSFill size={30} />
+            </button>
+            <button
+              ref={nextRef}
+              className="bg-white text-black h-8 w-8 shadow-lg flex justify-center items-center z-10 hover:scale-110 transition duration-300 ease-in-out"
+            >
+              <RiArrowRightSFill size={30} />
+            </button>
           </div>
-        ))}
-      </div>
-    ):(
-    <div className="relative mt-2">
-      <div className='float-end flex gap-2 mb-5'>
-        <button
-          ref={prevRef}
-          className="bg-white text-black h-8 w-8 shadow-lg p-2 flex justify-center items-center z-10 hover:scale-110 transition duration-300 ease-in-out"
-        >
-          <MdArrowBackIos size={20} />
-        </button>
-        <button
-          ref={nextRef}
-          className="bg-white text-black h-8 w-8 shadow-lg p-2 flex justify-center items-center z-10 hover:scale-110 transition duration-300 ease-in-out"
-        >
-          <MdArrowForwardIos size={20} />
-        </button>
-      </div>
-      <Swiper
-        ref={swiperRef}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        breakpoints={{
-          320: {
-            slidesPerView: 1.3,
-            spaceBetween: 10,
-          },
-          370: {
-            slidesPerView: 2,
-            spaceBetween: 10,
-          },
-          640: {
-            slidesPerView: 2.2,
-            spaceBetween: 20,
-          },
-          768: {
-            slidesPerView: 2.6,
-            spaceBetween: 10,
-          },
-          1024: {
-            slidesPerView: 3.2,
-            spaceBetween: 20,
-          },
-          1220: {
-            slidesPerView: 4,
-            spaceBetween: 20,
-          },
-        }}
-        modules={[Autoplay, Pagination, Navigation]}
-        pagination={{
-          dynamicBullets: true,
-          clickable: true,
-          renderBullet: function (index: number, className: string) {
-            return `<span class="custom-pag ${className}">${index + 1}</span>`;
-          },
-        }}
-        navigation={{
-          prevEl: prevRef.current,
-          nextEl: nextRef.current,
-        }}
-        className="mySwiper custom"
-      >
-        {featuredProducts.map((product, index) => (
-          <SwiperSlide key={index} className="mb-10 custom">
-            <Card ProductCard={[product]} slider={true} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
-    )}
-</>
+          <Swiper
+            ref={swiperRef}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            breakpoints={{
+              320: {
+                slidesPerView: 1.3,
+                spaceBetween: 10,
+              },
+              370: {
+                slidesPerView: 2,
+                spaceBetween: 10,
+              },
+              640: {
+                slidesPerView: 2.2,
+                spaceBetween: 20,
+              },
+              768: {
+                slidesPerView: 2.6,
+                spaceBetween: 10,
+              },
+              1024: {
+                slidesPerView: 3.2,
+                spaceBetween: 20,
+              },
+              1220: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+              },
+            }}
+            modules={[Autoplay, Pagination, Navigation]}
+            pagination={{
+              dynamicBullets: true,
+              clickable: true,
+              renderBullet: function (index: number, className: string) {
+                return `<span class="custom-pag ${className}">${index + 1}</span>`;
+              },
+            }}
+            navigation={{
+              prevEl: prevRef.current,
+              nextEl: nextRef.current,
+            }}
+            className="mySwiper custom"
+          >
+            {featuredProducts.map((product, index) => (
+              <SwiperSlide key={index} className="mb-10 custom">
+                <Card ProductCard={[product]} slider={true} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      )}
+    </>
 
 
   );
