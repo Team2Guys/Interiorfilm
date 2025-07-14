534d1dc0c43401d7f013464da427793b8f49ade3
AhmedSaddique <ahmed.saddique12@gmail.com>
2025-07-14 15:11:33 +0500

    image comprress

--- src/components/PDF/PDF.tsx
+++ src/components/PDF/PDF.tsx
@@ -9,7 +9,7 @@ const PDF = () => {
     <Container className='grid grid-cols-12 pt-5 pb-5 md:pb-0 items-center justify-items-center relative'>
         <div className='col-span-12 md:col-span-7 relative'>
           <div className=' w-[300px] h-[300px] sm:w-[500px] sm:h-[500px] lg:w-[800px] lg:h-[800px]'>
-            <Image fill quality={75} src="/images/pdf.png" alt='pdf' loading='lazy'/>
+            <Image fill quality={75} src="/images/pdf.webp" alt='pdf' loading='lazy'/>
           </div>
         </div>
         <div className='md:col-span-1'/>
